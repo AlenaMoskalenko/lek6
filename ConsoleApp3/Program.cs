d411e788a1083ee5dec21581d123f32095d6490d
alenarizhova <alenarizhova@gmail.com>
2019-02-28 22:12:11 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    interface IGeometrical // объявление интерфейса
+    {
+        void GetPerimeter();
+        void GetArea();
+    }
+    class Rectangle : IGeometrical //реализация интерфейса
+    {
+        public void GetPerimeter()
+        {
+            Console.WriteLine("Rectangle GetPerimeter (a+b)*2");
+        }
+
+        public void GetArea()
+        {
+            Console.WriteLine("Rectangle GetArea a*b");
+        }
+    }
+    class Circle : IGeometrical //реализация интерфейса
+    {
+
+        public void GetPerimeter()
+        {
+            Console.WriteLine("Circle GetPerimeter 2*pi*r");
+        }
+
+        public void GetArea()
+        {
+            Console.WriteLine("Circle GetArea pi*r^2");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IGeometrical> figures = new List<IGeometrical>();
+            figures.Add(new Rectangle());
+            figures.Add(new Circle());
+            foreach (IGeometrical f in figures)
+            {
+                f.GetPerimeter();
+                f.GetArea();
+            }
+            Console.ReadLine();
+        }
+    }
+}
